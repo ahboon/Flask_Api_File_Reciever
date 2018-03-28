22beaea48aa5417b4aab0bf437969a1699a8a486
Cher Boon <simcherboon123@gmail.com>
2018-03-28 13:29:27 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, jsonify, make_response
+from flask import request, abort, redirect,url_for
+from werkzeug.utils import secure_filename
+from time import gmtime, strftime
+from datetime import datetime
+
+
+UPLOAD_FOLDER = 'path\to\file'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','mp3','m4a'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = str(datetime.now().strftime("%y-%m-%d-%H-%M-%S")) + "-" + secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
+
